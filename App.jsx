fe1ba451de50ba06a26be7b6aeeb4ef347390a14
Enis Yavuz <enis.1739@gmail.com>
2023-02-04 20:41:28 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import {
   StyleSheet,
-  Text,
   View,
 } from 'react-native';
 
 import Header from './components/Header';
-
 import Footer from './components/Footer';
 
 function App(props) {
